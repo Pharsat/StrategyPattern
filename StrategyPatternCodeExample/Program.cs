a4a8ef704ba35311e9ef93cb628f84bc6c381049
Cristian Felipe Gallego Ramírez <Cristian.Felipe.Gallego.Ramirez@ey.com>
2020-03-19 19:38:01 -0500

    Se remueve la implamentacion en consola por la implementacion de las pruebas unitarias.

--- StrategyPatternCodeExample/Program.cs
+++ StrategyPatternCodeExample/Program.cs
@@ -6,30 +6,7 @@ namespace StrategyPatternCodeExample
     {
         static void Main(string[] args)
         {
-            var context = new AntivirusContext();
 
-            Console.WriteLine("Press 1 for complete execution or 2 for simple execution.");
-
-            switch (Console.ReadKey().Key)
-            {
-                case ConsoleKey.D1:
-                case ConsoleKey.NumPad1:
-                    context.SetStrategy(new CompleteAnalysis());
-                    context.ExecuteScan();
-                    break;
-
-                case ConsoleKey.D2:
-                case ConsoleKey.NumPad2:
-                    context.SetStrategy(new SimpleAnalysis());
-                    context.ExecuteScan();
-                    break;
-
-                default:
-                    context.SetStrategy(new SimpleAnalysis());
-                    context.ExecuteScan();
-                    break;
-            }
-            Console.ReadLine();
         }
     }
 }
